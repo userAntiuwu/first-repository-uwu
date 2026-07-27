2bf77d6d47cfffc7cf049ae030190cdb6628c7fa
UserAntiuwu <system.out.leon@gmail.com>
2026-07-26 19:40:17 -0600

    Add progress message to codigo.py
    Added a new print statement to indicate progress.

--- codigo.py
+++ codigo.py
@@ -4,3 +4,4 @@ apellido = "Leon"
 saludo = "hola "+ nombre +" "+apellido+" ¿como estas?"
 print(saludo)
 print("espero que ahora se me haga mas facil :3")
+print("mmm creo que ahora si lo tengo :3")
